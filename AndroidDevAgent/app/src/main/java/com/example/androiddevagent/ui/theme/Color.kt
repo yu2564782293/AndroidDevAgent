c2852198fbd91c303353ca9d64cb5735e3ac1b52
yu2564782293 <yu2564782293@users.noreply.github.com>
2026-06-03 05:37:13 +0000

    fix: 修复主题文件(Color.kt/Theme.kt/Type.kt)和编译类型错误

---
+++ AndroidDevAgent/app/src/main/java/com/example/androiddevagent/ui/theme/Color.kt
@@ -0,0 +1,69 @@
+package com.example.androiddevagent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF0D47A1)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFFCFD8DC)
+val OnPrimaryContainer = Color(0xFF0A1929)
+
+val Secondary = Color(0xFF00BFA5)
+val OnSecondary = Color(0xFFFFFFFF)
+val SecondaryContainer = Color(0xFFE0F7FA)
+val OnSecondaryContainer = Color(0xFF00251A)
+
+val Tertiary = Color(0xFF7C4DFF)
+val OnTertiary = Color(0xFFFFFFFF)
+val TertiaryContainer = Color(0xFFE8DDFF)
+val OnTertiaryContainer = Color(0xFF1E004D)
+
+val Error = Color(0xFFBA1A1A)
+val OnError = Color(0xFFFFFFFF)
+val ErrorContainer = Color(0xFFFFDAD6)
+val OnErrorContainer = Color(0xFF410002)
+
+val Background = Color(0xFFF5F7FA)
+val OnBackground = Color(0xFF1A1C1E)
+val Surface = Color(0xFFFFFFFF)
+val OnSurface = Color(0xFF1A1C1E)
+
+val SurfaceVariant = Color(0xFFE7E8ED)
+val OnSurfaceVariant = Color(0xFF44474F)
+
+val Outline = Color(0xFF74777F)
+val OutlineVariant = Color(0xFFC4C6D0)
+
+val DarkPrimary = Color(0xFF82B1FF)
+val DarkOnPrimary = Color(0xFF002E5C)
+val DarkPrimaryContainer = Color(0xFF004282)
+val DarkOnPrimaryContainer = Color(0xFFCFD8DC)
+
+val DarkSecondary = Color(0xFF00E5C3)
+val DarkOnSecondary = Color(0xFF003731)
+val DarkSecondaryContainer = Color(0xFF004F49)
+val DarkOnSecondaryContainer = Color(0xFFE0F7FA)
+
+val DarkTertiary = Color(0xFFB47CFF)
+val DarkOnTertiary = Color(0xFF2D0060)
+val DarkTertiaryContainer = Color(0xFF43218A)
+val DarkOnTertiaryContainer = Color(0xFFE8DDFF)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+val DarkErrorContainer = Color(0xFF93000A)
+val DarkOnErrorContainer = Color(0xFFFFDAD6)
+
+val DarkBackground = Color(0xFF0A1628)
+val DarkOnBackground = Color(0xFFE2E2E6)
+val DarkSurface = Color(0xFF0F1E36)
+val DarkOnSurface = Color(0xFFE2E2E6)
+
+val DarkSurfaceVariant = Color(0xFF444750)
+val DarkOnSurfaceVariant = Color(0xFFC4C6D0)
+
+val DarkOutline = Color(0xFF8E9099)
+val DarkOutlineVariant = Color(0xFF444750)
+
+val DerekGradientStart = Color(0xFF0D47A1)
+val DerekGradientEnd = Color(0xFF00BFA5)
+val DerekAccent = Color(0xFF7C4DFF)
