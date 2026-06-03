02bc00f845b6c27a67273f43333d94eba69dae7b
yu2564782293 <2564782293@qq.com>
2026-06-03 12:05:13 +0800

    Remove unused Room type converters annotation

--- AndroidDevAgent/app/src/main/java/com/example/androiddevagent/data/ProjectDatabase.kt
+++ AndroidDevAgent/app/src/main/java/com/example/androiddevagent/data/ProjectDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.example.androiddevagent.data.dao.ProjectDao
 import com.example.androiddevagent.data.entity.Project
 
@@ -13,7 +12,6 @@ import com.example.androiddevagent.data.entity.Project
     version = 1,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class ProjectDatabase : RoomDatabase() {
     
     abstract fun projectDao(): ProjectDao
